238abc63f23d86f2ef56ec3b1e75eff3124b14a5
Tim Samson <9625746+timsamson@users.noreply.github.com>
2021-01-07 15:27:19 -0500

    plot code

--- static/js/app.js
+++ static/js/app.js
@@ -17,16 +17,11 @@ updatePlots(0)
 // Function for updating plots   
 function updatePlots(index) {
 
-
     // Set up arrays for horizontal bar chart & gauge chart
     var sampleSubjectOTUs = data.samples[index].otu_ids;
-    console.log(sampleSubjectOTUs);
     var sampleSubjectFreq = data.samples[index].sample_values;
     var otuLabels = data.samples[index].otu_labels;
-
     var washFrequency = data.metadata[+index].wfreq;
-    console.log(washFrequency);
-
 
     // Populate Demographic Data card
     var demoKeys = Object.keys(data.metadata[index]);
@@ -35,12 +30,9 @@ function updatePlots(index) {
 
     // clear demographic data
     demographicData.html("");
-
     for (var i = 0; i < demoKeys.length; i++) {
-
       demographicData.append("p").text(`${demoKeys[i]}: ${demoValues[i]}`);
     };
-
 
     // Slice and reverse data for horizontal bar chart
     var topTenOTUS = sampleSubjectOTUs.slice(0, 10).reverse();
@@ -49,31 +41,116 @@ function updatePlots(index) {
     var topTenLabels = topTenOTUS.map((otu => "OTU " + otu));
     var reversedLabels = topTenLabels.reverse();
 
-    // Set up trace
-    var trace1 = {
-      x: topTenFreq,
-      y: reversedLabels,
-      text: topTenToolTips,
-      name: "",
-      type: "bar",
-      orientation: "h"
-    };
-
-    // data
-    var barData = [trace1];
-
-    // Apply  layout
-    var layout = {
-      title: "Top 10 OTUs",
-      margin: {
-        l: 75,
-        r: 75,
-        t: 75,
-        b: 50
+     // Chart Traces
+     //Bar
+    var traceBar = {
+        x: topTenFreq,
+        y: reversedLabels,
+        text: topTenToolTips,
+        name: "",
+        type: "bar",
+        orientation: "h"
       }
-    };
- // Render the plot to the div tag with id "plot"
- Plotly.newPlot("bar", barData, layout);
 
- 
+    //Bubble
+    var traceBubble = {
+        x: sampleSubjectOTUs,
+        y: sampleSubjectFreq,
+        text: otuLabels,
+        mode: 'markers',
+        marker: {
+          color: sampleSubjectOTUs,
+          opacity: [1, 0.8, 0.6, 0.4],
+          size: sampleSubjectFreq
+        }
+      }
 
+    //Guage
+    var traceGuage = [{
+        domain: {x: [0, 1], y: [0,1]},
+        type: "indicator",
+        mode: "gauge+number",
+        value: washFrequency,
+        title: { text: "Belly Button Washes Per Week" },
+        gauge: {
+          axis: { range: [0, 9], tickwidth: 0.5, tickcolor: "black" },
+          bar: { color: "#669999" },
+          bgcolor: "white",
+          borderwidth: 2,
+          bordercolor: "transparent",
+          steps: [
+            { range: [0, 1], color: "#fff" },
+            { range: [1, 2], color: "#e6fff5" },
+            { range: [2, 3], color: "ccffeb" },
+            { range: [3, 4], color: "b3ffe0" },
+            { range: [4, 5], color: "#99ffd6" },
+            { range: [5, 6], color: "#80ffcc" },
+            { range: [6, 7], color: "#66ffc2" },
+            { range: [7, 8], color: "#4dffb8" },
+            { range: [8, 9], color: "#33ffad" } 
+          ],
+        }
+      }];
+
+    //Chart Data Vars
+      var barData = [traceBar];
+      var bubbleData = [traceBubble];
+      var gaugeData = traceGuage;      
+      
+    //Chart Layouts
+    //Bar
+      var layoutBar = {
+        title: "Top 10 OTUs",
+        margin: {
+          l: 75,
+          r: 75,
+          t: 75,
+          b: 50
+        }
+      };
+    
+    //Bubble
+      var layoutBubble = {
+        title: 'OTU Frequency',
+        showlegend: false,
+        height: 600,
+        width: 930
+      }
+    
+    //Guage
+      var layoutGuage = {
+        width: 600,
+        height: 500,
+        margin: { t: 0, b: 0 }
+      };
+
+    // Render the plot to the div tag with id "plot"
+        Plotly.newPlot("bar", barData, layoutBar);
+  
+    // Render the plot to the div tag with id "bubble-plot"
+        Plotly.newPlot("bubble", bubbleData, layoutBubble)
+        
+    // Render the plot to the div tag with id "guage"   
+        Plotly.newPlot("gauge", gaugeData, layoutGuage);
+    }
+  
+    // On button click, call refreshData()
+    d3.selectAll("#selDataset").on("change", refreshData);
+
+    function refreshData() {
+      var dropdownMenu = d3.select("#selDataset");
+      // Assign the value of the dropdown menu option to a variable
+      var personsID = dropdownMenu.property("value");
+      console.log(personsID);
+      // Initialize an empty array for the person's data
+      console.log(data)
+  
+      for (var i = 0; i < data.names.length; i++) {
+        if (personsID === data.names[i]) {
+          updatePlots(i);
+          return
+        }
+      }
+    }
+  
+  });
